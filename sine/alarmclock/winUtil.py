18115d30a70f5a13826933a461f33e5f84c9f6cc
Sine <714186139@qq.com>
2020-04-18 17:17:19 +0800

    v1.0.7 优化了windows窗口相关功能，避免启动后偶尔失效的问题
    开机时可能启动太早，或者说模块加载延迟，可能获取不到窗口句柄，导致窗口功能失效

--- sine/alarmclock/winUtil.py
+++ sine/alarmclock/winUtil.py
@@ -7,6 +7,9 @@ windows 窗口相关的辅助接口函数。
 
 import win32api
 import win32gui
+from . import mylogging
+
+logger = mylogging.getLogger(__name__)
 
 hWnd = 0
 
@@ -16,6 +19,16 @@ def refreshWindow():
     s = win32api.GetConsoleTitle()
     hWnd = win32gui.FindWindow(0, s)
 
+def refreshOnError(func):
+    def refreshOnErrorWrapper(*args, **kw):
+        try:
+            return func(*args, **kw)
+        except Exception as e:
+            logger.warn("run window function fail. trying again...", exc_info=1)
+            return func(*args, **kw)
+    return refreshOnErrorWrapper
+
+@refreshOnError
 def flash(dwFlags=3, uCount=5, dwTimeout=500):
     '''
     @see  http://timgolden.me.uk/pywin32-docs/win32gui__FlashWindowEx_meth.html
@@ -37,22 +50,27 @@ def flash(dwFlags=3, uCount=5, dwTimeout=500):
     '''
     win32gui.FlashWindowEx(hWnd, dwFlags, uCount, dwTimeout)
 
+@refreshOnError
 def stopFlash():
     '''stop the flashing before it finish, but not discard the highlight.'''
     flash(0, 0, 0)
 
+@refreshOnError
 def show(swFlag):
     win32gui.ShowWindow(hWnd, swFlag)
 
+@refreshOnError
 def isForeground():
     return win32gui.GetForegroundWindow() == hWnd
 
+@refreshOnError
 def setForeground():
     try:
         win32gui.SetForegroundWindow(hWnd)
     except Exception as e:
         pass
 
+@refreshOnError
 def bringToTop():
     win32gui.BringWindowToTop(hWnd)
 
